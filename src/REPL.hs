6226426456f15d93a15eaac162fd4ed91bb63d0d
amanjpro <amanj.sherwany@usi.ch>
2017-02-05 12:53:02 -0500

    Make the parser actually work

--- src/REPL.hs
+++ src/REPL.hs
@@ -4,16 +4,20 @@ import System.Posix.Types
 import System.Directory
 import Control.Monad.Trans.Reader
 import Control.Monad.IO.Class
-import Text.Parsec
+import Text.Parsec hiding (token)
 import Text.Parsec.String
 import Text.Parsec.Token
 import Text.Parsec.Language
 import Control.Applicative ((<$>), (<*))
+import Data.String.Utils
 
 
-data Joint      = Pipe | Read | Write | Append
-data Cmd        = Cmd {cmd :: String, args :: [String]} |
-                  ComplexCmd {cmd1 :: Cmd, joint :: Joint, cmd2 :: Cmd}
+data Action     = Read | Write | Append deriving (Show, Eq)
+data Joint      = Pipe | And deriving (Show, Eq)
+data Cmd        = SimpleCmd String [String] |
+                  RedirectCmd Cmd Action String |
+                  CompoundCmd Cmd Joint Cmd |
+                  SubShell Cmd deriving (Show, Eq)
 newtype History = History {history :: [Cmd]}
 
 callSystem :: String -> [String] -> IO ()
@@ -26,6 +30,7 @@ repl :: History -> IO ()
 repl history = do
   dir  <- getCurrentDirectory
   putStr $ dir ++ "$ "
+  cmd <- getCmd
   line <- getLine
   case line of
     "exit"       -> return ()
@@ -36,7 +41,7 @@ repl history = do
         childPID <- exec cmd args []
         wait childPID
         return ()
-      repl $ extend (Cmd cmd args) history
+      repl $ extend (SimpleCmd cmd args) history
       where tokens = words line
             cmd    = head tokens
             args   = tail tokens
@@ -64,29 +69,114 @@ wait childPID =
          Nothing -> fail "Error: Nothing from getProcessStatus"
          Just ps -> return ps
 
+-- IO
+
+data CmdString = PartialCmd String | CompleteCmd String
+
+
+getCommandString :: String -> String -> [Char] -> CmdString
+getCommandString original ""             []          = CompleteCmd original
+getCommandString original ""             _           = PartialCmd original
+getCommandString original "\\"           []          = PartialCmd $ init original
+getCommandString original "\""           []          = PartialCmd original
+getCommandString original "'"            []          = PartialCmd original
+getCommandString original ('\\':'\"':xs) qts         = getCommandString original xs qts
+getCommandString original ('\\':'\'':xs) qts         = getCommandString original xs qts
+getCommandString original ('"':xs)       ('"':rest)  = getCommandString original xs rest
+getCommandString original ('"':xs)       qts         = getCommandString original xs ('"':qts)
+getCommandString original ('\'':xs)      ('\'':rest) = getCommandString original xs rest
+getCommandString original ('\'':xs)      qts         = getCommandString original xs ('\'':qts)
+getCommandString original (x:xs)         qts         = getCommandString original xs qts
+
+
+processLine :: String -> IO Cmd
+processLine prefix = do
+  let s = if null prefix then "" else "> "
+  putStr s
+  line <- getLine
+  let fullLine = prefix ++ line
+  case getCommandString fullLine fullLine [] of
+    CompleteCmd cmd -> return $ parseCmd cmd
+    PartialCmd  cmd -> processLine cmd
+
+getCmd :: IO Cmd
+getCmd = processLine ""
 
 -- parser
+
+toJoint :: String -> Joint
+toJoint "|"  = Pipe
+toJoint "&&" = And
+
+
+toAction :: String -> Action
+toAction "<"  = Read
+toAction ">"  = Write
+toAction ">>" = Append
+
 token :: Parser String
 token = do
   spaces
   haskellString <|> many1 alphaNum
   where haskellString = stringLiteral $ makeTokenParser haskellDef
 
-jointParser :: Parser String
-jointParser = pipe <|> read <|> write <|> append
-  where pipe   = string "|"
-        read   = string "<"
+jointParser :: Parser Joint
+jointParser = do
+  spaces
+  link <- pipe <|> andThen
+  return $ toJoint link
+  where pipe       = string "|"
+        andThen    = string "&&"
+
+actionParser :: Parser (Action, String)
+actionParser = do
+  spaces
+  link <- read <|> write <|> append
+  spaces
+  file <- token
+  let action = toAction link
+  return (action, file)
+  where read   = string "<"
         write  = string ">"
         append = string ">>"
 
+
+
+simpleCmdParser :: Parser Cmd
+simpleCmdParser = do
+  command  <- token
+  args     <- many token
+  let cmd = SimpleCmd command args
+  action   <- optionMaybe actionParser
+  let res = case action of
+              Nothing             -> cmd
+              Just (redirect, file) -> RedirectCmd cmd redirect file
+  return res
+
+compoundCmdParser :: Parser Cmd
+compoundCmdParser = do
+  fst  <- simpleCmdParser
+  link <- jointParser
+  snd  <- compoundCmdParser
+  return $ CompoundCmd fst link snd
+
 cmdParser :: Parser Cmd
-cmdParser = do
-  cmd  <- token
-  args <- many token
-  return $ Cmd cmd args
+cmdParser = simpleCmdParser <|> compoundCmdParser
 
-cmdsParser :: Parser Cmd
-cmdsParser = do
-  cmds <- cmd <|> many $ cmd <$> joint <$> cmd
-  return Cmd "" []
+subShellParser :: Parser Cmd
+subShellParser = do
+  spaces
+  char '('
+  cmd <- cmdParser
+  spaces
+  char ')'
+  return $ SubShell cmd
 
+
+entryParser :: Parser Cmd
+entryParser = subShellParser <|> cmdParser
+
+parseCmd :: String -> Cmd
+parseCmd input = case parse entryParser "(unknown)" input of
+                   Right cmd -> cmd
+                   Left  err -> error $ show err
