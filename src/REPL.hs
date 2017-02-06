75c2e86f241d986af271c525e462620fcf42eaf4
amanjpro <amanj.sherwany@usi.ch>
2017-02-05 22:11:41 -0500

    Make the shell semi fully functional

--- src/REPL.hs
+++ src/REPL.hs
@@ -1,15 +1,31 @@
 module REPL(shell) where
+
+-- Posix modules
 import System.Posix.Process
 import System.Posix.Types
+import System.Posix.IO
+import System.Posix.Files
+
+-- System modules
+import System.Exit
 import System.Directory
-import Control.Monad.Trans.Reader
-import Control.Monad.IO.Class
-import Text.Parsec hiding (token)
+import System.IO
+
+-- Parser modules
+import Text.Parsec
 import Text.Parsec.String
 import Text.Parsec.Token
-import Text.Parsec.Language
 import Control.Applicative ((<$>), (<*))
+
+-- Util modules
 import Data.String.Utils
+import Control.Exception.Base (catch)
+
+-- Concurrency
+import Control.Concurrent
+import Control.Concurrent.MVar
+
+type CloseFds = MVar [Fd]
 
 
 data Action     = Read | Write | Append deriving (Show, Eq)
@@ -21,30 +37,21 @@ data Cmd        = SimpleCmd String [String] |
 newtype History = History {history :: [Cmd]}
 
 callSystem :: String -> [String] -> IO ()
-callSystem "cd" args = setCurrentDirectory $ head args
+callSystem "exit" []    = exitSuccess
+callSystem "cd"   [arg] = setCurrentDirectory arg
 
 builtinCommands :: [String]
-builtinCommands = ["cd"]
+builtinCommands = ["exit", "cd"]
 
 repl :: History -> IO ()
 repl history = do
   dir  <- getCurrentDirectory
   putStr $ dir ++ "$ "
   cmd <- getCmd
-  line <- getLine
-  case line of
-    "exit"       -> return ()
-    otherwise    -> do
-      if elem cmd builtinCommands then callSystem cmd args
-      else do
-        -- Fork the child
-        childPID <- exec cmd args []
-        wait childPID
-        return ()
-      repl $ extend (SimpleCmd cmd args) history
-      where tokens = words line
-            cmd    = head tokens
-            args   = tail tokens
+  closefds <- newMVar []
+  res <- runCommand cmd closefds ""
+  putStr res
+  repl $ extend cmd history
 
 shell :: IO ()
 shell = repl newHistory
@@ -55,6 +62,73 @@ extend cmd hist = History $ cmd:cmds
 
 newHistory :: History
 newHistory = History []
+
+
+runCommand :: Cmd -> CloseFds -> String -> IO String
+runCommand (SimpleCmd cmd args) closefds input          =
+  if cmd `elem` builtinCommands then
+    do callSystem cmd args
+       return ""
+  else do
+    (stdinread, stdinwrite) <- createPipe
+    (stdoutread, stdoutwrite) <- createPipe
+    addCloseFDs closefds [stdinwrite, stdoutread]
+    childId <- withMVar closefds (\fds -> forkProcess (runChild cmd args fds stdinread stdoutwrite))
+    closeFd stdinread
+    closeFd stdoutwrite
+    stdinhdl <- fdToHandle stdinwrite
+    forkIO $ do hPutStr stdinhdl input
+                hClose stdinhdl
+    stdouthdl <- fdToHandle stdoutread
+    wait childId
+    handle <- fdToHandle stdoutread
+    hGetContents handle
+runCommand (RedirectCmd cmd Read file) closefds input   = do
+  content <- readFile file
+  runCommand cmd closefds content
+runCommand (RedirectCmd cmd Write file) closefds input  = do
+  res <- runCommand cmd closefds input
+  writeFile file res
+  return ""
+runCommand (RedirectCmd cmd Append file) closefds input = do
+  res <- runCommand cmd closefds input
+  appendFile file res
+  return ""
+runCommand (CompoundCmd cmd1 And cmd2) closefds input   = do
+  runCommand cmd1 closefds input
+  runCommand cmd2 closefds ""
+runCommand (CompoundCmd cmd1 Pipe cmd2) closefds input = do
+  res <- runCommand cmd1 closefds input
+  runCommand cmd2 closefds res
+
+runChild :: String -> [String] -> [Fd] -> Fd -> Fd -> IO ()
+runChild cmd args closefds input output = do
+  dupTo input stdInput
+  dupTo output stdOutput
+  closeFd input
+  closeFd output
+  let _ = closeFd `map` closefds
+  executeFile cmd True args Nothing
+
+-- Add FDs to the list of FDs that must be closed post-fork in a child
+addCloseFDs :: CloseFds -> [Fd] -> IO ()
+addCloseFDs closefds newfds =
+    modifyMVar_ closefds (\oldfds -> return $ oldfds ++ newfds)
+
+-- Remove FDs from the list
+removeCloseFDs :: CloseFds -> [Fd] -> IO ()
+removeCloseFDs closefds removethem =
+    modifyMVar_ closefds (\fdlist -> return $ procfdlist fdlist removethem)
+
+    where
+    procfdlist fdlist [] = fdlist
+    procfdlist fdlist (x:xs) = procfdlist (removefd fdlist x) xs
+
+    -- We want to remove only the first occurance ot any given fd
+    removefd [] _ = []
+    removefd (x:xs) fd
+        | fd == x = xs
+        | otherwise = x : removefd xs fd
 
 
 exec :: String -> [String] -> [(String, String)] -> IO ProcessID
@@ -114,11 +188,34 @@ toAction "<"  = Read
 toAction ">"  = Write
 toAction ">>" = Append
 
-token :: Parser String
-token = do
+legalChar :: Parser Char
+legalChar = (oneOf ['.', '/', ':', '-']) <|> digit <|> letter
+
+
+singleQuote :: Parser String
+singleQuote = do
+  string "\""
+  contents <- many (((:[]) <$> noneOf ['\'']) <|> string "\\'")
+  string "\""
+  let content = foldl (++) "" contents
+  return $ "\"" ++ content ++ "\""
+
+doubleQuote :: Parser String
+doubleQuote = do
+  string "'"
+  contents <- many (((:[]) <$> noneOf ['\'']) <|> string "\\'")
+  string "'"
+  let content = foldl (++) "" contents
+  return $ "'" ++ content ++ "'"
+
+
+shellString :: Parser String
+shellString = singleQuote <|> doubleQuote
+
+tokenParser :: Parser String
+tokenParser = do
   spaces
-  haskellString <|> many1 alphaNum
-  where haskellString = stringLiteral $ makeTokenParser haskellDef
+  many1 legalChar <|> shellString
 
 jointParser :: Parser Joint
 jointParser = do
@@ -128,14 +225,15 @@ jointParser = do
   where pipe       = string "|"
         andThen    = string "&&"
 
-actionParser :: Parser (Action, String)
+actionParser :: Parser Cmd
 actionParser = do
+  command  <- cmdParser
   spaces
   link <- read <|> write <|> append
   spaces
-  file <- token
+  file <- tokenParser
   let action = toAction link
-  return (action, file)
+  return $ RedirectCmd command action file
   where read   = string "<"
         write  = string ">"
         append = string ">>"
@@ -144,14 +242,9 @@ actionParser = do
 
 simpleCmdParser :: Parser Cmd
 simpleCmdParser = do
-  command  <- token
-  args     <- many token
-  let cmd = SimpleCmd command args
-  action   <- optionMaybe actionParser
-  let res = case action of
-              Nothing             -> cmd
-              Just (redirect, file) -> RedirectCmd cmd redirect file
-  return res
+  command  <- tokenParser
+  args     <- many tokenParser
+  return $ SimpleCmd command args
 
 compoundCmdParser :: Parser Cmd
 compoundCmdParser = do
@@ -161,20 +254,23 @@ compoundCmdParser = do
   return $ CompoundCmd fst link snd
 
 cmdParser :: Parser Cmd
-cmdParser = simpleCmdParser <|> compoundCmdParser
+cmdParser = try compoundCmdParser <|> simpleCmdParser
 
 subShellParser :: Parser Cmd
 subShellParser = do
   spaces
   char '('
-  cmd <- cmdParser
+  cmd <- entryParser
   spaces
   char ')'
   return $ SubShell cmd
 
 
 entryParser :: Parser Cmd
-entryParser = subShellParser <|> cmdParser
+entryParser = do
+  res <- subShellParser <|> try actionParser <|> cmdParser
+  eof
+  return res
 
 parseCmd :: String -> Cmd
 parseCmd input = case parse entryParser "(unknown)" input of
